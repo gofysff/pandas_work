902d4c31d82f1698a23e2599dd5b509d4defa5de
ZgeraldZ <80286126+ZgeraldZ@users.noreply.github.com>
2023-02-28 21:03:53 +0300

    little bit comma

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 
 def load_data():
+    # возвращает dataframe с нашими данными
+    # где ключ, это наш первый столбик
     return pd.read_excel('assets/lab1_air_pollution.xlsx', index_col=0)
 
 
